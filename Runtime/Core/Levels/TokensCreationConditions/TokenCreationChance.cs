7f68388d553c49312625e6918e4f4fb02d7d99f9
mnicolas94 <miguelcok27@gmail.com>
2022-08-23 16:49:06 -0400

    FEAT: implement TokenCreationChance

---
+++ Runtime/Core/Levels/TokensCreationConditions/TokenCreationChance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Match3.Core.Levels.TokensCreationConditions
+{
+    [Serializable]
+    public class TokenCreationChance : ITokenCreationPredicate
+    {
+        [SerializeField, Range(0.0f, 1.0f)] private float _chance;
+        
+        public bool IsMet((GameController, TokenSource, Vector2Int) input)
+        {
+            return Random.value <= _chance;
+        }
+    }
+}
